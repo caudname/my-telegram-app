3ab2114d34cfbbd1f13ba6a73a9cb7cdd33a65d1
Said <khasbulatov.vniigaz@gmail.com>
2025-09-18 12:17:38 +0300

    sensors params added

--- src/components/Example/Example.tsx
+++ src/components/Example/Example.tsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
-import { DndContext, type DragEndEvent } from '@dnd-kit/core'
+import { DndContext, type DragEndEvent, PointerSensor, useSensor, useSensors } from '@dnd-kit/core'
 import { Draggable } from '../Draggable'
 import { Droppable } from '../Droppable'
 
 export const Example: React.FC = () => {
   const [ parent, setParent ] = useState<string | null>(null)
+
+  const sensors = useSensors(
+    useSensor(PointerSensor, {
+      activationConstraint: {
+        distance: 5   // optional: how far finger must move before drag starts
+      }
+    })
+  )
 
   const handleDragEnd = ({ over }: DragEndEvent) => {
     setParent(over ? over.id.toString() : null)
@@ -17,7 +25,7 @@ export const Example: React.FC = () => {
   )
 
   return (
-    <DndContext onDragEnd={ handleDragEnd }>
+    <DndContext onDragEnd={ handleDragEnd } sensors={ sensors }>
       { !parent ? draggable : null }
       <Droppable id='droppable'>
         { parent === "droppable" ? draggable : 'Drop here' }
